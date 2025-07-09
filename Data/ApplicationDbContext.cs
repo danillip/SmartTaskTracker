9ec499815d0ab182a52d92a2a533d04c935ccb17
GreateMaple <162444795+danillip@users.noreply.github.com>
2025-07-09 15:11:08 +0300

    Revert "Add event/task management with tests"

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,17 +1,13 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using SmartTaskTracker.Models;
 
-namespace SmartTaskTracker.Data;
-
-public class ApplicationDbContext : IdentityDbContext<AppUser, IdentityRole<int>, int>
+namespace SmartTaskTracker.Data
 {
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options)
+    public class ApplicationDbContext : IdentityDbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
     }
-
-    public DbSet<Event> Events => Set<Event>();
-    public DbSet<TaskItem> TaskItems => Set<TaskItem>();
 }
--- Models/AppUser.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.AspNetCore.Identity;
-
-namespace SmartTaskTracker.Models;
-
-public class AppUser : IdentityUser<int>
-{
-}
